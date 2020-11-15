1dbb366ba8fc8de9ec0196d648c9cf03ca4c3647
Ian Skelskey <46094112+IanSkelskey@users.noreply.github.com>
2020-11-15 01:56:25 -0500

    Add files via upload
    -Changed from drawing rectangles to drawing lines with strokes for the brush tool.
    ----->Brush creates "smooth" lines now without gaps.
    -Added GUI components for rectangle tool

--- Whiteboard Clone 2/src/whiteboard/clone/pkg2/FXMLDocumentController.java
+++ Whiteboard Clone 2/src/whiteboard/clone/pkg2/FXMLDocumentController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package whiteboard.clone.pkg2;
 
 import java.net.URL;
@@ -13,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -22,40 +22,59 @@ import javafx.scene.control.TextField;
  * @author Ian A Skelskey
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private ColorPicker colorPicker;
-    
+
     @FXML
     private TextField bsize;
-    
+
     @FXML
     private Canvas canvas;
-    
-    
+
+    @FXML
+    private CheckBox chkBrush;
+    private CheckBox chkRectangle;
+
     boolean toolSelected = true;
-    
-    
-    
+
     GraphicsContext brushTool;
-    
+
+    private double lastX;
+    private double lastY;
+    private double size;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         brushTool = canvas.getGraphicsContext2D();
-        canvas.setOnMouseDragged(e-> {
-            double size = Double.parseDouble(bsize.getText());
-            double x = e.getX() - size/2;
-            double y = e.getY() - size/2;
-            
-            if(toolSelected && !bsize.getText().isEmpty()){
-                brushTool.setFill(colorPicker.getValue());
-                brushTool.fillRoundRect(x, y, size, size, size, size);
-            }
+        
+        canvas.setOnMousePressed(e -> {
+            lastX = e.getX();
+            lastY = e.getY();
         });
-    }    
-    
+
+        canvas.setOnMouseDragged(e -> {
+            
+            double size = Double.parseDouble(bsize.getText());
+            double x = e.getX() - size / 2;
+            double y = e.getY() - size / 2;
+
+            brushTool.setLineWidth(size);
+            brushTool.setStroke(colorPicker.getValue());
+            brushTool.strokeLine(lastX, lastY, x, y);
+            lastX = x;
+            lastY = y;
+
+        });
+    }
+
     @FXML
-    public void toolSelected(ActionEvent e){
+    public void undo() {
+        //undo function here
+    }
+
+    @FXML
+    public void toolSelected(ActionEvent e) {
         toolSelected = true;
     }
 }
